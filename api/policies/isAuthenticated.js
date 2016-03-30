a0b22f3d9b5415256fa7ee312c23db7a57093548
Makah <arieiramau@gmail.com>
2016-03-30 15:14:32 +0000

    Criar uma mensagem de erro para o usuário, informando que ele não está logado

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -1,9 +1,13 @@
 
 module.exports = function(req, res, next) {
-   if (req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         return next();
     }
-    else{
-        return res.redirect('/');
-    }
+    
+    //Chrome bug POST same page twice, so I prefer to override the message
+    //req.flash('error', 'You are not permitted to perform this action');
+    if(!req.session.flash) req.session.flash = {}; 
+    req.session.flash.error = ['You are not permitted to perform this action'];
+    
+    return res.redirect(307, '/');
 };
