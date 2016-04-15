cdb3298827f27ea2a5e0c4b630f6cb25861dbfe4
Makah <arieiramau@gmail.com>
2016-04-15 02:22:13 +0000

    Change config.appUrl to environment + Organize Passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,23 +2,44 @@ var passport = require('passport'),
 LocalStrategy = require('passport-local').Strategy,
 bcrypt = require('bcrypt');
 
+/////////////////////////
+/// Initialize
+///////////////////////// 
+var locals = require('./local');
+var appUrl = process.env.APP_URL || 'localhost:1337';
+
+if(typeof locals === 'undefined'){
+  console.log('[ERROR]', "config/local.js not found.");
+  return;
+}
+
+/////////////////////////
+/// CORE
+/////////////////////////
 passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
     User.findOne({ id: id } , function (err, user) {
-        done(err, user);
+    if (err)
+      sails.log.error('passport.deserializeUser: ', 'ID', id);
+
+    done(err, user);
     });
 });
 
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-  },
-  function(email, password, done) {
+/////////////////////////
+/// Local
+/////////////////////////
+var localOptions = {
+  usernameField: 'email',
+  passwordField: 'password'
+};
 
-    User.findOne({ email: email }, function (err, user) {
+passport.use(new LocalStrategy(localOptions, function(email, password, done) {
+  
+  User.findOne({ email: email }, function (err, user) {
       if (err) {
           return done(err);
       }
